b0efc1bd8e7919732b79ee48f82a5112bcae88d0
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-19 01:37:10 -0300

    feat: CustomUserDetailsService

---
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.login_auth_api.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
